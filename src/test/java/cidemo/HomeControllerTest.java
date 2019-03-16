bc67d8f7bb0da58bdc9d6ffc1d7abc8493a3222d
Hans Westerbeek <hans@enhance-ict.com>
2019-03-16 18:18:59 +0100

    add some coverage

--- src/test/java/cidemo/HomeControllerTest.java
+++ src/test/java/cidemo/HomeControllerTest.java
@@ -2,11 +2,23 @@ package cidemo;
 
 import static org.junit.Assert.*;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import cidemo.HomeController.Msg;
+
+@RunWith(MockitoJUnitRunner.class)
 public class HomeControllerTest {
+    private static final String MSG = "foo";
+
+    private HomeController subject = new HomeController(MSG);
 
     @Test
     public void spillTheBeans() {
+
+        final Msg msg = subject.spillTheBeans();
+        assertThat(msg.oops, Matchers.equalTo(MSG));
     }
 }
