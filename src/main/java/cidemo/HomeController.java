0bb4a302291debb06d395b6038477b65bf48758c
Hans Westerbeek <hans@enhance-ict.com>
2019-04-23 15:52:10 +0200

    Read from db

--- src/main/java/cidemo/HomeController.java
+++ src/main/java/cidemo/HomeController.java
@@ -2,8 +2,9 @@ package cidemo;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Collections;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,18 +12,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class HomeController {
-    private final String mySecret;
 
-    public HomeController(@Value("${my.secret}") String mySecret) {
 
-        this.mySecret = mySecret;
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    public HomeController(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     @GetMapping
     public Msg spillTheBeans() throws UnknownHostException { // don't try this at home
 
         String hostname = InetAddress.getLocalHost().getHostName();
-        return new Msg(mySecret + ", from: " + hostname);
+        return new Msg(String.format("Hello from %s, running on: %s", this.getFromDb(), hostname));
+    }
+
+    private String getFromDb() {
+        return this.jdbcTemplate.queryForObject("SELECT first_name, lastt_name FROM person WHERE first_name='Dave'",
+                Collections.emptyMap(),
+                (rs, rowNum) -> rs.getString(1) + " " + rs.getString(2));
     }
 
     public static class Msg {
--- src/test/java/cidemo/HomeControllerTest.java
+++ src/test/java/cidemo/HomeControllerTest.java
@@ -1,14 +1,20 @@
 package cidemo;
 
-import static org.hamcrest.Matchers.startsWith;
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 
 import java.net.UnknownHostException;
+import java.util.Map;
 
-import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import cidemo.HomeController.Msg;
 
@@ -16,12 +22,19 @@ import cidemo.HomeController.Msg;
 public class HomeControllerTest {
     private static final String MSG = "foo";
 
-    private HomeController subject = new HomeController(MSG);
+    @Mock
+    NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    @InjectMocks
+    private HomeController subject;
 
     @Test
-    public void spillTheBeans() throws UnknownHostException {
+    public void obligatory() throws UnknownHostException {
+
+        Mockito.when(namedParameterJdbcTemplate.queryForObject(
+                any(String.class), any(Map.class), any(RowMapper.class))).thenReturn(MSG + " bar");
 
         final Msg msg = subject.spillTheBeans();
-        assertThat(msg.msg, startsWith(MSG));
+        assertThat(msg.msg, containsString(MSG));
     }
 }
